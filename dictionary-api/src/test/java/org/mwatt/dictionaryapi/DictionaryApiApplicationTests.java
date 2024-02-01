10af310882d8eb4ec7462bb2c74bd1e7ed3abdf0
murray-watt <149982805+Murray-Watt@users.noreply.github.com>
2024-02-01 09:52:07 -0700

    upload initial source

---
+++ dictionary-api/src/test/java/org/mwatt/dictionaryapi/DictionaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mwatt.dictionaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
